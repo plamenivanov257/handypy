436dea92eb797df68a602f0bb9ac742cead28310
plamenivanov257 <plamen.ivanov257@gmail.com>
2024-12-11 15:16:41 +0100

    debug plots

--- plots/debug_plots.py
+++ plots/debug_plots.py
@@ -25,17 +25,42 @@ class DebugFigHandler:
         return fig_index
 
 
-    def subplots(self, fig_index, *args):
+    def subplots(self, fig_index=0, nrows=1, ncols=1, **kwargs):
+        if len(self._figs) == 0 and fig_index == 0:
+            print("Have no debug figs... Creating one now!")
+            self.new_debug_fig()
+
         if self._closed[fig_index]:
             print(f"Debug figure {fig_index} is no longer open! Cannot add subplots!")
-            return
+            return None
 
         self._figs[fig_index].clf()
-        self._figs[fig_index].subplots(*args)
+        return self._figs[fig_index].subplots(nrows=nrows, ncols=ncols, **kwargs)
 
 
-    def update_fig(self, fig_index):
+    def update_fig(self, fig_index=0):
         if self._closed[fig_index]:
             print(f"Debug figure {fig_index} is no longer open! Cannot update it!")
         self._figs[fig_index].canvas.draw()
         input(f"Updated debug figure {fig_index}. Press enter to continue.")
+
+
+    def close_figs(self):
+        for fig in self._figs:
+            plt.close(fig)
+
+
+    def __init__(self, n=0):
+        """
+        Creates a handler and initialises n debug figs.
+        """
+        for i in range(n):
+            self.new_debug_fig()
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.close_figs()
